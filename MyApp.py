330d40808a7d07f10338d9c645a23af4bc03c775
Naoki Inomata <soramoyou04@gmail.com>
2023-01-29 10:38:07 +0900

    first commit

---
+++ MyApp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Streamlit 超入門')
+
+st.write('DetaFrame')
+
+df = pd.DataFrame(
+  np.random.rand(20,3),
+  columns=['a', 'b', 'c']
+)
+# st.dataframe(df)
