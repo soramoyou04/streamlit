92718fd0cd7ada648ee7ba048dae0ff63f127976
Naoki Inomata <soramoyou04@gmail.com>
2023-01-29 18:18:03 +0900

    画像追加

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import os
 from PIL import Image
 
+path = os.path.dirname(__file__)
 st.title('Streamlit 超入門')
 st.write('MOCHI')
-img=Image.open("moti.JPG")
+img=Image.open(f"{path}/moti.JPG")
 st.image(img,caption='もち',use_column_width=True)
