c97b1e3799bcef178f76292b51fa55840864c49f
Naoki Inomata <soramoyou04@gmail.com>
2023-01-29 20:23:57 +0900

    画像追加

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import streamlit as st
 
-st.title('Streamlit 超入門')
-st.write('MOCHI')
+st.title('もち')
 
 button=st.button('もちの写真')
 
-st.image('https://github.com/soramoyou04/streamlit/blob/master/moti.jpg?raw=true',caption='もち',use_column_width=True)
+# if button:
+#     # img=Image.open('moti2.JPG')
+#     # st.image(img,caption='もち',use_column_width=True)
 
-if button:
-    col1, col2, col3 = st.columns(3)
-    ex=st.expander('ex')
-    ex.write("A")
-    ex.image('https://github.com/soramoyou04/streamlit/blob/master/moti.jpg?raw=true', use_column_width=True)
+#     col1, col2, col3 = st.columns(3)
 
-    with col1:
-      st.header("A")
-      st.image('https://github.com/soramoyou04/streamlit/blob/master/moti.jpg?raw=true', use_column_width=True)
+#     ex=st.expander('ex')
+#     ex.write("A")
+#     ex.image("https://github.com/happyyumeno/moti/blob/master/moti.JPG?raw=true",use_column_width=True)
+    
 
-    with col2:
-      st.header("B")
-      st.image('https://github.com/soramoyou04/streamlit/blob/master/moti.jpg?raw=true', use_column_width=True)
+#     with col1:
+#         st.header("A")
+#         st.image("https://github.com/happyyumeno/moti/blob/master/moti.JPG?raw=true", use_column_width=True)
+        
 
-button=st.button('とじる')
-"""
-# もちかわいい
-## もち可愛い
-### もちすごい可愛いよね？
-"""
+#     with col2:
+#         st.header("B")
+#         st.image("https://github.com/happyyumeno/moti/blob/master/moti2.JPG?raw=true", use_column_width=True)
+    
+#     with col3:
+#         st.header("C")
+#         st.image("https://github.com/happyyumeno/moti/blob/master/moti3.jpg?raw=true", use_column_width=True)
+
+# button=st.button('とじる')
+# # if button:
+#     # img=Image.close('')
+
+# """
+# # もちかわいい
+# ## もち可愛い
+# ### もちすごい可愛いよね？
+# """
